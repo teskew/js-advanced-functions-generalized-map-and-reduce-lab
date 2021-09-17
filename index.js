495368a0ef35b7a15433ae5aa85a9c702d692cc8
github username <github email address>
2021-09-17 11:38:19 -0500

    commit tree 23c2bbd8a5e53eaf1e589f45cfece71c7a3432bc

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, func ) {
+    let newArr = []
+    arr.forEach(e =>  {
+        newArr.push(func(e))
+
+    });
+    return newArr
+}
+
+function reduce(arr, func, start) {
+    let r = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+    for (; i < arr.length; i++){
+        r = func(arr[i], r)
+    }
+    return r;
+    }
+    
